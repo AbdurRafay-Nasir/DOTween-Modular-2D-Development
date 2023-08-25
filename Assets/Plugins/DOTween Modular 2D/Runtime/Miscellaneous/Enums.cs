8ae11993d0b3d353b85aeab0e9e202a51543e205
Linked-Games <linkedgames07@gmail.com>
2023-08-25 18:32:21 +0500

    Fixed Code Formatting of Miscellaneous Classes

--- Assets/Plugins/DOTween Modular 2D/Runtime/Miscellaneous/Enums.cs
+++ Assets/Plugins/DOTween Modular 2D/Runtime/Miscellaneous/Enums.cs
@@ -1,53 +1,52 @@
-namespace DOTweenModular2D.Enums
-{
-
 using UnityEngine;
 
-public enum Begin
+namespace DOTweenModular2D.Enums
 {
-    [Tooltip("Start as soon game runs")]
-    OnSceneStart,
+    public enum Begin
+    {
+        [Tooltip("Start as soon game runs")]
+        OnSceneStart,
 
-    [Tooltip("Start only When visible for first time")]
-    OnVisible,
+        [Tooltip("Start only When visible for first time")]
+        OnVisible,
 
-    [Tooltip("Start when something enters the trigger")]
-    OnTrigger,
+        [Tooltip("Start when something enters the trigger")]
+        OnTrigger,
 
-    [Tooltip("Will not start on its own, you have to either manually call CreateTween() & " + "\n" +
-             "Tween.Play(), or use this componenet in a sequence component")]
-    Manual,
+        [Tooltip("Will not start on its own, you have to either manually call CreateTween() & " + "\n" +
+                 "Tween.Play(), or use this componenet in a sequence component")]
+        Manual,
     
-    [Tooltip("Start AFTER Tween Object's tween is completed")]
-    After,
+        [Tooltip("Start AFTER Tween Object's tween is completed")]
+        After,
 
-    [Tooltip("Start when Tween Object's tween is created")]
-    With
-}
+        [Tooltip("Start when Tween Object's tween is created")]
+        With
+    }
 
-public enum Kill
-{
-    OnTweenComplete, OnInvisible, Manual
-}
+    public enum Kill
+    {
+        OnTweenComplete, OnInvisible, Manual
+    }
 
-public enum TweenType
-{
-    Simple, Looped    
-}
+    public enum TweenType
+    {
+        Simple, Looped    
+    }
 
-public enum ApplyTo
-{
-    Position, Rotation, Scale
-}
+    public enum ApplyTo
+    {
+        Position, Rotation, Scale
+    }
 
-public enum LookAtPath
-{
-    None, Position, Transform, Percentage
-}
+    public enum LookAtPath
+    {
+        None, Position, Transform, Percentage
+    }
 
-public enum LookAtSimple
-{
-    None, Position, Transform
-}
+    public enum LookAtSimple
+    {
+        None, Position, Transform
+    }
 
 }
