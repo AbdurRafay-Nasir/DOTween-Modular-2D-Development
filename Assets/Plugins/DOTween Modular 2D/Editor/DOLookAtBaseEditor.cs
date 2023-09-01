d357e2f67236fdd6a2621a767aff1406870363e0
Linked-Games <linkedgames07@gmail.com>
2023-09-01 11:01:14 +0500

    Added DOLookAtBaseEditor, DOLookAtEditor now inherits it

---
+++ Assets/Plugins/DOTween Modular 2D/Editor/DOLookAtBaseEditor.cs
@@ -0,0 +1,152 @@
+#if UNITY_EDITOR
+
+using UnityEditor;
+using UnityEngine;
+
+namespace DOTweenModular2D.Editor
+{
+    public class DOLookAtBaseEditor : DOBaseEditor
+    {
+        private SerializedProperty lookAtProp;
+        private SerializedProperty lookAtTargetProp;
+        private SerializedProperty lookAtPositionProp;
+        private SerializedProperty offsetProp;
+        private SerializedProperty minProp;
+        private SerializedProperty maxProp;
+        private SerializedProperty smoothFactorProp;
+
+        #region Foldout Bool
+
+        protected bool lookAtSettingsFoldout = true;
+        protected string savedLookAtSettingsFoldout;
+
+        #endregion
+
+        private DOLookAt dOLookAt;
+
+        #region Inspector Draw Functions
+
+        protected void DrawLookAtSettings()
+        {
+            EditorGUILayout.PropertyField(lookAtProp);
+
+            if (dOLookAt.lookAt == Enums.LookAtSimple.None)
+                return;
+
+            switch (dOLookAt.lookAt)
+            {
+                case Enums.LookAtSimple.Position:
+                    EditorGUILayout.PropertyField(lookAtPositionProp);
+                    break;
+
+                case Enums.LookAtSimple.Transform:
+                    EditorGUILayout.PropertyField(lookAtTargetProp);
+                    break;
+            }
+            EditorGUILayout.PropertyField(offsetProp);
+            EditorGUILayout.PropertyField(smoothFactorProp);
+            EditorGUILayout.PropertyField(minProp);
+            EditorGUILayout.PropertyField(maxProp);
+        }
+
+        protected void DrawLookAtHelpBox()
+        {
+            if (dOLookAt.lookAt == Enums.LookAtSimple.Transform && dOLookAt.lookAtTarget == null)
+            {
+                EditorGUILayout.HelpBox("Look At Target not Assigned", MessageType.Error);
+            }
+            else if (dOLookAt.lookAt != Enums.LookAtSimple.Transform && dOLookAt.lookAtTarget != null)
+            {
+                EditorGUILayout.BeginHorizontal();
+
+                EditorGUILayout.HelpBox("Look At Target is still Assigned, it Should be removed", MessageType.Warning);
+
+                GUIContent trashButton = EditorGUIUtility.IconContent("TreeEditor.Trash");
+                trashButton.tooltip = "Remove Look At Target";
+
+                if (GUILayout.Button(trashButton, GUILayout.Height(buttonSize), GUILayout.Width(buttonSize * 2f)))
+                {
+                    dOLookAt.lookAtTarget = null;
+                }
+
+                EditorGUILayout.EndHorizontal();
+            }
+        }
+
+        #endregion
+
+        #region Scene View Draw Functions
+
+        protected void DrawLookAtHandle()
+        {
+            Vector2 newLookAtPosition = Handles.PositionHandle(dOLookAt.lookAtPosition, Quaternion.identity);
+
+            if (newLookAtPosition != dOLookAt.lookAtPosition)
+            {
+                Undo.RecordObject(dOLookAt, "Change Look At Position_DOLookAt");
+                dOLookAt.lookAtPosition = newLookAtPosition;
+            }
+        }
+
+        protected void DrawRotationClampCircle()
+        {
+            Vector3 position = dOLookAt.transform.position;
+
+            // Calculate the endpoints of the arc based on the min and max angles
+            float minAngle = (dOLookAt.min + 90) * Mathf.Deg2Rad;
+            float maxAngle = (dOLookAt.max + 90) * Mathf.Deg2Rad;
+            Vector3 minDir = new Vector3(Mathf.Cos(minAngle), Mathf.Sin(minAngle), 0);
+            Vector3 maxDir = new Vector3(Mathf.Cos(maxAngle), Mathf.Sin(maxAngle), 0);
+
+            // Draw the circle representing the range
+            Handles.DrawWireArc(position, Vector3.forward, minDir, dOLookAt.max - dOLookAt.min, 2f);
+
+            // Draw lines from the center to the min and max angles
+            Handles.DrawLine(position, position + minDir * 2f);
+            Handles.DrawLine(position, position + maxDir * 2f);
+        }
+
+        protected void DrawLookAtLine()
+        {
+            if (dOLookAt.lookAt == Enums.LookAtSimple.Position)
+            {
+                Handles.DrawDottedLine(dOLookAt.transform.position, dOLookAt.lookAtPosition, 5f);
+            }
+            else if (dOLookAt.lookAtTarget != null)
+            {
+                Handles.DrawDottedLine(dOLookAt.transform.position, dOLookAt.lookAtTarget.position, 5f);
+            }
+        }
+
+        #endregion
+
+        #region Setup Functions
+
+        protected override void SetupSerializedProperties()
+        {
+            base.SetupSerializedProperties();
+
+            lookAtProp = serializedObject.FindProperty("lookAt");
+            lookAtTargetProp = serializedObject.FindProperty("lookAtTarget");
+            lookAtPositionProp = serializedObject.FindProperty("lookAtPosition");
+            offsetProp = serializedObject.FindProperty("offset");
+            smoothFactorProp = serializedObject.FindProperty("smoothFactor");
+            minProp = serializedObject.FindProperty("min");
+            maxProp = serializedObject.FindProperty("max");
+        }
+
+        protected override void SetupSavedVariables(DOBase doLookAt)
+        {
+            base.SetupSavedVariables(doLookAt);
+            dOLookAt = (DOLookAt)doLookAt;
+            int instanceId = doLookAt.GetInstanceID();
+
+            savedLookAtSettingsFoldout = "DOLookAtBaseEditor_lookAtSettingsFoldout_" + instanceId;
+            lookAtSettingsFoldout = EditorPrefs.GetBool(savedLookAtSettingsFoldout, true);
+        }
+
+        #endregion
+    }
+}
+
+#endif
--- Assets/Plugins/DOTween Modular 2D/Editor/DOLookAtEditor.cs
+++ Assets/Plugins/DOTween Modular 2D/Editor/DOLookAtEditor.cs
@@ -6,28 +6,8 @@ using UnityEditor;
 namespace DOTweenModular2D.Editor
 {
     [CustomEditor(typeof(DOLookAt)), CanEditMultipleObjects]
-    public class DOLookAtEditor : DOBaseEditor
+    public class DOLookAtEditor : DOLookAtBaseEditor
     {
-
-        #region Serialized Properties
-
-        public SerializedProperty lookAtProp;
-        public SerializedProperty lookAtTargetProp;
-        public SerializedProperty lookAtPositionProp;
-        public SerializedProperty offsetProp;
-        public SerializedProperty minProp;
-        public SerializedProperty maxProp;
-        public SerializedProperty smoothFactorProp;
-
-        #endregion
-
-        #region Foldout Bool
-
-        private bool lookAtSettingsFoldout = true;
-        private string savedLookAtSettingsFoldout;
-
-        #endregion
-
         private DOLookAt doLookAt;
 
         private bool[] tabStates = new bool[4];
@@ -100,26 +80,7 @@ namespace DOTweenModular2D.Editor
                 EditorGUILayout.EndFoldoutHeaderGroup();
             }
 
-            if (doLookAt.lookAt == Enums.LookAtSimple.Transform && doLookAt.lookAtTarget == null)
-            {
-                EditorGUILayout.HelpBox("Look At Target not Assigned", MessageType.Error);
-            }
-            else if (doLookAt.lookAt != Enums.LookAtSimple.Transform && doLookAt.lookAtTarget != null)
-            {
-                EditorGUILayout.BeginHorizontal();
-
-                EditorGUILayout.HelpBox("Look At Target is still Assigned, it Should be removed", MessageType.Warning);
-
-                GUIContent trashButton = EditorGUIUtility.IconContent("TreeEditor.Trash");
-                trashButton.tooltip = "Remove Look At Target";
-
-                if (GUILayout.Button(trashButton, GUILayout.Height(buttonSize), GUILayout.Width(buttonSize * 2f)))
-                {
-                    doLookAt.lookAtTarget = null;
-                }
-
-                EditorGUILayout.EndHorizontal();
-            }
+            DrawLookAtHelpBox();
 
             if (tabStates[2])
             {
@@ -182,6 +143,9 @@ namespace DOTweenModular2D.Editor
 
         private void OnSceneGUI()
         {
+            if (doLookAt.lookAt == Enums.LookAtSimple.None)
+                return;
+
             if (doLookAt.begin == Enums.Begin.After ||
                 doLookAt.begin == Enums.Begin.With)
             {
@@ -190,9 +154,6 @@ namespace DOTweenModular2D.Editor
                 if (doLookAt.tweenObject != null)
                     DrawTweenObjectInfo();
             }
-
-            if (doLookAt.lookAt == Enums.LookAtSimple.None)
-                return;
 
             Handles.color = Color.green;
 
@@ -204,51 +165,6 @@ namespace DOTweenModular2D.Editor
             DrawRotationClampCircle();
 
             DrawLookAtLine();
-        }
-
-        #endregion
-
-        #region Scene View Draw Functions
-
-        private void DrawLookAtHandle()
-        {
-            Vector2 newLookAtPosition = Handles.PositionHandle(doLookAt.lookAtPosition, Quaternion.identity);
-
-            if (newLookAtPosition != doLookAt.lookAtPosition)
-            {
-                Undo.RecordObject(doLookAt, "Change Look At Position_DOLookAt");
-                doLookAt.lookAtPosition = newLookAtPosition;
-            }
-        }
-
-        private void DrawRotationClampCircle()
-        {
-            Vector3 position = doLookAt.transform.position;
-
-            // Calculate the endpoints of the arc based on the min and max angles
-            float minAngle = (doLookAt.min + 90) * Mathf.Deg2Rad;
-            float maxAngle = (doLookAt.max + 90) * Mathf.Deg2Rad;
-            Vector3 minDir = new Vector3(Mathf.Cos(minAngle), Mathf.Sin(minAngle), 0);
-            Vector3 maxDir = new Vector3(Mathf.Cos(maxAngle), Mathf.Sin(maxAngle), 0);
-
-            // Draw the circle representing the range
-            Handles.DrawWireArc(position, Vector3.forward, minDir, doLookAt.max - doLookAt.min, 2f);
-
-            // Draw lines from the center to the min and max angles
-            Handles.DrawLine(position, position + minDir * 2f);
-            Handles.DrawLine(position, position + maxDir * 2f);
-        }
-
-        private void DrawLookAtLine()
-        {
-            if (doLookAt.lookAt == Enums.LookAtSimple.Position)
-            {
-                Handles.DrawDottedLine(doLookAt.transform.position, doLookAt.lookAtPosition, 5f);
-            }
-            else if (doLookAt.lookAtTarget != null)
-            {
-                Handles.DrawDottedLine(doLookAt.transform.position, doLookAt.lookAtTarget.position, 5f);
-            }
         }
 
         #endregion
@@ -284,54 +200,15 @@ namespace DOTweenModular2D.Editor
             EditorGUILayout.PropertyField(durationProp);
         }
 
-        private void DrawLookAtSettings()
-        {
-            EditorGUILayout.PropertyField(lookAtProp);
-
-            if (doLookAt.lookAt == Enums.LookAtSimple.None)
-                return;
-
-            switch (doLookAt.lookAt)
-            {
-                case Enums.LookAtSimple.Position:
-                    EditorGUILayout.PropertyField(lookAtPositionProp);
-                    break;
-
-                case Enums.LookAtSimple.Transform:
-                    EditorGUILayout.PropertyField(lookAtTargetProp);
-                    break;
-            }
-            EditorGUILayout.PropertyField(offsetProp);
-            EditorGUILayout.PropertyField(smoothFactorProp);
-            EditorGUILayout.PropertyField(minProp);
-            EditorGUILayout.PropertyField(maxProp);
-        }
-
         #endregion
 
         #region Setup Functions
-
-        protected override void SetupSerializedProperties()
-        {
-            base.SetupSerializedProperties();
-
-            lookAtProp = serializedObject.FindProperty("lookAt");
-            lookAtTargetProp = serializedObject.FindProperty("lookAtTarget");
-            lookAtPositionProp = serializedObject.FindProperty("lookAtPosition");
-            offsetProp = serializedObject.FindProperty("offset");
-            smoothFactorProp = serializedObject.FindProperty("smoothFactor");
-            minProp = serializedObject.FindProperty("min");
-            maxProp = serializedObject.FindProperty("max");
-        }
 
         protected override void SetupSavedVariables(DOBase doLookAt)
         {
             base.SetupSavedVariables(doLookAt);
 
             int instanceId = doLookAt.GetInstanceID();
-
-            savedLookAtSettingsFoldout = "DOLookAtEditor_lookAtSettingsFoldout_" + instanceId;
-            lookAtSettingsFoldout = EditorPrefs.GetBool(savedLookAtSettingsFoldout, true);
 
             for (int i = 0; i < savedTabStates.Length; i++)
             {
